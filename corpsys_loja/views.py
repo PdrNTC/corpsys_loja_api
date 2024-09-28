64388cc91dc9ab9c5a047b4b35b6a3400e8fbed0
Pedro Hernandes <thepedrinhoo@gmail.com>
2024-09-28 18:53:24 -0300

    Criando as ViewSets e configurando endpoints

--- corpsys_loja/views.py
+++ corpsys_loja/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from corpsys_loja.models import Cliente, Vendedor, Produto, GrupoProduto, Venda, ItemVenda
+from corpsys_loja.serializers import ClienteSerializer, VendedorSerializer, ProdutoSerializer, GrupoProdutoSerializer, VendaSerializer, ItemVendaSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class VendedorViewSet(viewsets.ModelViewSet):
+    queryset = Vendedor.objects.all()
+    serializer_class = VendedorSerializer
+
+class ProdutoViewSet(viewsets.ModelViewSet):
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+
+class GrupoProdutoViewSet(viewsets.ModelViewSet):
+    queryset = GrupoProduto.objects.all()
+    serializer_class = GrupoProdutoSerializer
+
+class VendaViewSet(viewsets.ModelViewSet):
+    queryset = Venda.objects.all()
+    serializer_class = VendaSerializer
+
+class ItemVendaViewSet(viewsets.ModelViewSet):
+    queryset = ItemVenda.objects.all()
+    serializer_class = ItemVendaSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from corpsys_loja.views import ClienteViewSet, VendedorViewSet, ProdutoViewSet, GrupoProdutoViewSet, VendaViewSet, ItemVendaViewSet
+
+# Criando obj router #
+router = routers.DefaultRouter()
+router.register(r'clientes', ClienteViewSet)
+router.register(r'vendedores', VendedorViewSet)
+router.register(r'produtos', ProdutoViewSet)
+router.register(r'grupo-produtos', GrupoProdutoViewSet)
+router.register(r'vendas', VendaViewSet)
+router.register(r'itens-venda', ItemVendaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)), # Incluindo objeto com todas rotas
 ]
