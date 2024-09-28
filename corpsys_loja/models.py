6e53c7ef99c27754f9e2a2c7f505ab27c518e5cf
Pedro Hernandes <thepedrinhoo@gmail.com>
2024-09-28 18:24:46 -0300

    Configuracoes para mudar o db para MySQL

--- corpsys_loja/models.py
+++ corpsys_loja/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True) # Permitindo somente emails unicos
+    telefone = models.CharField(max_length=20)
+
+class Vendedor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
