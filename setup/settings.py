a3057478e9e3ec1c855c70b547b7d09c6215167d
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-09-30 14:27:41 -0300

    Permissao de acesso a api corsheaders

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corpsys_loja', # Adicionando o APP
     'rest_framework', # Adicionando o DRF
+    'corsheaders', # liberar acesso api
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # CORSHEADERS permitir conexao #
 ]
+
+# Permitir todos os domínios 
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'setup.urls'
 
